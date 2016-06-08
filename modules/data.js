270888a6c473522186ddff2390e92a95bd5ed64a
Oskenso Kashi <contact@oskenso.com>
2016-06-08 23:34:27 +0100

    starting point...

--- modules/data.js
+++ modules/data.js
@@ -1,10 +1,21 @@
 this.run = pixelData => {
+    console.log(pixelData.dimensions);
+    var width = pixelData.dimensions.width;
+    var height = pixelData.dimensions.height;
+
+    for (var i = 0; i < pixelData.pixels.length; i++) {
+        var x = i % width;
+        var y = Math.floor(i / width);
+
+    }
+    /*
     for (var i = 0; i < pixelData.pixels.length; i++) {
         var rgb = `${ pixelData.pixels[i].r }, ${ pixelData.pixels[i].g }, ${ pixelData.pixels[i].b }`;
         if (rgb !== pixelData.primaryColor) {
 
         }
     }
+    */
 }
 
 this.createObject = data => {
