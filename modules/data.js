765bb89c1592dab5b46a631e29abfb35d0e0bb1f
Oskenso Kashi <contact@oskenso.com>
2016-06-09 16:32:32 -0700

    test for stuffs :3

--- modules/data.js
+++ modules/data.js
@@ -3,11 +3,38 @@ this.run = pixelData => {
     var width = pixelData.dimensions.width;
     var height = pixelData.dimensions.height;
 
+    var Locations = new Array();
+    console.log('Mapping pixel data...');
+    var start = Date.now();
+
+    var deltaPixelGrid = new Array();
     for (var i = 0; i < pixelData.pixels.length; i++) {
         var x = i % width;
         var y = Math.floor(i / width);
+        var pixel = pixelData.pixels[i];
+        var rgb = [pixel.r << 16 | pixel.g << 8 | pixel.b];
+
+        var pixelGrid = new Array();
+
+        pixelGrid[0] = rgb;
+        for (var ii = 1; ii < pixelGrid.length; ii++) {
+            var nextPixel = pixelData.pixels[ii];
+            pixelGrid[ii] = [nextPixel.r << 16 | nextPixel.g << 8 | nextPixel.b]
+        }
+
+        deltaPixelGrid = pixelGrid;
+
+        Locations.push(pixelGrid);
+
+
+        //Locations.push(location);
 
     }
+    //console.log(Locations);
+
+    console.log('Pixel Grids: ' + Locations.length);
+    var end = Date.now();
+    console.log(`Finished in ${ end - start }ms.`);
     /*
     for (var i = 0; i < pixelData.pixels.length; i++) {
         var rgb = `${ pixelData.pixels[i].r }, ${ pixelData.pixels[i].g }, ${ pixelData.pixels[i].b }`;
